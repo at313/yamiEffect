be492bc4b255478691dc0582bcf939940289a084
Taiki Arakawa <at.090313@gmail.com>
2016-08-26 10:41:14 +0900

    設定完了

---
+++ src/resource.js
@@ -0,0 +1,26 @@
+var res = {
+  chara_princessselect_10: "res/char/chara_princessselect_10.png",
+  chara_princessselect_11: "res/char/chara_princessselect_11.png",
+  chara_princessselect_12: "res/char/chara_princessselect_12.png",
+  ss_BattleScene_bg1: "res/field/ss_BattleScene_bg1.jpg",
+  ss_BattleScene_bg2: "res/field/ss_BattleScene_bg2.jpg",
+  ss_BattleScene_bg3: "res/field/ss_BattleScene_bg3.jpg",
+  chara_enemy_1: "res/char/chara_enemy_1.png",
+  chara_enemy_2: "res/char/chara_enemy_2.png",
+  chara_enemy_3: "res/char/chara_enemy_3.png",
+  chara_enemy_4: "res/char/chara_enemy_4.png",
+  chara_enemy_5: "res/char/chara_enemy_5.png",
+  chara_enemy_6: "res/char/chara_enemy_6.png",
+  darkness1_particl1: "res/effect/darkness_texture1.plist",
+  darkness1_particl2: "res/effect/darkness_texture2.plist",
+  darkness1_particl3: "res/effect/darkness_texture3.plist",
+  darkness1_particl4: "res/effect/darkness_texture4.plist",
+  darkness2_particl1: "res/effect/darkness2_texture1.plist",
+  darkness2_particl2: "res/effect/darkness2_texture2.plist",
+  darkness2_particl3: "res/effect/darkness2_texture3.plist"
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
